ba3528d3a79201700269ac0de842cc8c064bdd92
Vasyl Demchuk <coolvasyadem1@gmail.com>
2019-02-11 19:59:53 +0200

    debug

--- src/ch/makery/address/model/Person.java
+++ src/ch/makery/address/model/Person.java
@@ -28,8 +28,6 @@ public class Person {
     public Person(String firstName, String lastName) {
         this.firstName = new SimpleStringProperty(firstName);
         this.lastName = new SimpleStringProperty(lastName);
-
-        // Some initial dummy data, just for convenient testing.
         this.street = new SimpleStringProperty("some street");
         this.postalCode = new SimpleIntegerProperty(1234);
         this.city = new SimpleStringProperty("some city");
